b5e7e11ef05bbe82e4314f3156fa2194f377a19c
VV0212 <vigneshforall@gmail.com>
2026-04-19 00:04:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ filtered_df = df[
 # TITLE
 # -------------------------
 st.title("🏭 Industrial Sustainability Dashboard")
-st.markdown("### ⚡ Energy | 💧 Water | 🌍 GHG Monitoring System")
+st.markdown("### ⚡ Energy | 💧 Water | 🌍 GHG Emission)
 
 # -------------------------
 # KPI - PREDICTIONS
