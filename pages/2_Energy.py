a6f4874dc25ed0ce0ceff2502e1d1044ab72502e
VV0212 <vigneshforall@gmail.com>
2026-05-07 14:58:45 +0530

    Update 2_Energy.py

--- pages/2_Energy.py
+++ pages/2_Energy.py
@@ -3,29 +3,50 @@ import pandas as pd
 
 st.title("⚡ Energy Dashboard")
 
+# Load CSV
 df = pd.read_csv("data.csv")
+
+# Clean columns
 df.columns = df.columns.str.strip().str.lower()
+
+# Convert datetime
 df["datetime"] = pd.to_datetime(df["datetime"])
 
-# Filters (same as main)
+# Sidebar filters
 start_date = st.sidebar.date_input("Start Date")
 end_date = st.sidebar.date_input("End Date")
 
+# Filter dataframe
 filtered_df = df[
     (df["datetime"] >= pd.to_datetime(start_date)) &
     (df["datetime"] <= pd.to_datetime(end_date))
 ]
 
-energy = filtered_df["energy_intensity_kwh_per_unit"]
+# Energy data
+energy = filtered_df["energy_pred_kwh"]
+
+# KPI
+st.metric(
+    "Latest Energy",
+    f"{energy.iloc[-1]:.2f} kWh"
+)
 
 # Summary
 st.subheader("Energy Summary")
 st.write(energy.describe())
 
-# Data
+# Table
 st.subheader("Energy Data")
-st.dataframe(energy.head(20))
+st.dataframe(filtered_df[[
+    "datetime",
+    "energy_pred_kwh"
+]].head(20))
 
 # Chart
 st.subheader("Energy Trend")
-st.line_chart(filtered_df.set_index("datetime")["energy_intensity_kwh_per_unit"])
+
+st.line_chart(
+    filtered_df.set_index("datetime")[
+        "energy_pred_kwh"
+    ]
+)
